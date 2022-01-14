55cc9fa56abb098cfe3917a4109fa37fa49afd5f
Gabriel Laskar <gabriel.laskar@gmail.com>
2022-01-14 13:26:46 +0100

    refactor: push the main function out of tests, and introduce approvals

--- src/main/java/com/gildedrose/GildedRoseMain.java
+++ src/main/java/com/gildedrose/GildedRoseMain.java
@@ -1,8 +1,18 @@
 package com.gildedrose;
 
-public class TexttestFixture {
+import java.io.PrintStream;
+
+public class GildedRoseMain {
     public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+        int days = 2;
+        if (args.length > 0) {
+            days = Integer.parseInt(args[0]) + 1;
+        }
+        gildedRoseMain(System.out, days);
+    }
+
+    static void gildedRoseMain(final PrintStream out, int days) {
+        out.println("OMGHAI!");
 
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
@@ -18,18 +28,13 @@ public class TexttestFixture {
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            out.println("-------- day " + i + " --------");
+            out.println("name, sellIn, quality");
             for (Item item : items) {
-                System.out.println(item);
+                out.println(item);
             }
-            System.out.println();
+            out.println();
             app.updateQuality();
         }
     }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,11 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +16,16 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        //assertEquals("foo", app.items[0].name);
+        Approvals.verifyAll("items", app.items);
+    }
+
+    @Test
+    void approvalOriginalMain() {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outStream, true, StandardCharsets.UTF_8);
+        GildedRoseMain.gildedRoseMain(out, 2);
+        Approvals.verify(outStream);
     }
 
 }
