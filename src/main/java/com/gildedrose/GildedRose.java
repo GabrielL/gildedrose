1704777b2e65d5cfaf8f28cb9cc9461a36d4c6fc
Gabriel Laskar <gabriel.laskar@gmail.com>
2022-01-15 01:20:09 +0100

    refactor: simplify update

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,10 @@ class GildedRose {
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
+    static final int LOWEST_QUALITY = 0;
+    static final int HIGHEST_QUALITY = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,55 +20,51 @@ class GildedRose {
         }
     }
 
+    boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    void age(Item item) {
+        item.sellIn -= 1;
+    }
+
+    int changeQuality(int newQuality) {
+        return Math.min(HIGHEST_QUALITY, Math.max(LOWEST_QUALITY, newQuality));
+    }
+
     private void updateItemQuality(final Item item) {
         switch (item.name) {
             case SULFURAS_HAND_OF_RAGNAROS:
                 return;
             case AGED_BRIE:
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-                item.sellIn = item.sellIn - 1;
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                age(item);
+
+                if (isExpired(item)) {
+                    item.quality = changeQuality(item.quality + 2);
+                } else {
+                    item.quality = changeQuality(item.quality + 1);
                 }
                 break;
             case BACKSTAGE_PASSES:
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                age(item);
 
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = 0;
+                if (isExpired(item)) {
+                    item.quality = LOWEST_QUALITY;
+                } else if (item.sellIn < 5) {
+                    item.quality = changeQuality(item.quality + 3);
+                } else if (item.sellIn < 10) {
+                    item.quality = changeQuality(item.quality + 2);
+                } else {
+                    item.quality = changeQuality(item.quality + 1);
                 }
                 break;
             default:
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                age(item);
 
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                if (isExpired(item)) {
+                    item.quality = changeQuality(item.quality - 2);
+                } else {
+                    item.quality = changeQuality(item.quality - 1);
                 }
                 break;
         }
